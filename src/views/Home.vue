969004335c11125d2c4ab58ab342f0e1a4df3964
nmeinhold <43660864+NateMeinhold@users.noreply.github.com>
2019-04-28 15:53:44 -0700

    built and committing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,49 +1,93 @@
 <template>
   <div class="home">
-    <div class="form-container"><!-- TODO: Modulate display of the .form-container using v-show and the showForm variable. -->
+    <div class="form-container" v-show="showForm">
+      <!-- TODO: Modulate display of the .form-container using v-show and the showForm variable. -->
       <h1>Join the Web Developers Club!</h1>
       <p>Sign up to access our special, secret page. Just create an account and answer a brief survey.</p>
 
       <!-- TODO: Add an HTML element to display an error message for when the user submits invalid information. Use v-show to show/hide this message based on the validity of the form data. -->
+      <p
+        class="error"
+        v-show="showError"
+      >Please checked the information you have entered. Be sure to fill in all the fields.</p>
 
-      <form><!-- TODO: Add the proper v-on directive to let the validateForm method handle this form when it is submitted -->
+      <form v-on:submit.prevent="validateForm">
+        <!-- TODO: Add the proper v-on directive to let the validateForm method handle this form when it is submitted -->
 
         <!-- TODO: Add labels for each form field in this form. -->
+        <p>
+          <label for="username">
+            Username
+            <input type="text" id="username" v-model="username">
+          </label>
+        </p>
 
         <!-- TODO: Add a username field. -->
+        <p>
+          <label for="email">
+            Email
+            <input type="email" id="email" v-model="email">
+          </label>
+        </p>
 
         <!-- TODO: Add an email field. -->
+        <p>
+          <label for="password">
+            Password
+            <input type="password" id="password" v-model="password">
+          </label>
+        </p>
 
         <!-- TODO: Add a password field. -->
+        <p>
+          <label for="passwordVerify">
+            Confirm Password
+            <input type="password" id="passwordVerify" v-model="passwordVerify">
+          </label>
+        </p>
 
         <!-- TODO: Add a passwordVerify field. -->
 
-
-        <p><input type="submit" value="Submit"></p>
+        <p>
+          <input type="submit" value="Submit">
+        </p>
       </form>
     </div>
-    <div class="success-message"><!-- TODO: Modulate display of the .success-message using v-show and the showForm variable. -->
+    <div class="success-message" v-show="!showForm">
+      <!-- TODO: Modulate display of the .success-message using v-show and the showForm variable. -->
       <h1>Thank you for signing up!</h1>
-      <p>Please take our new member survey. Click here</p><!-- TODO: Link "Click here" to the survey page. -->
+      <p>Please take our new member survey.
+      <router-link to="/survey">Click here</router-link>
+      </p>
+      <!-- TODO: Link "Click here" to the survey page. -->
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home',
-  data () {
+  name: "Home",
+  data() {
     return {
-      username: '',
-      email: '',
-      password: '',
-      passwordVerify: '',
+      username: "",
+      email: "",
+      password: "",
+      passwordVerify: "",
       showForm: true,
       showError: false
-    }
+    };
   },
   methods: {
-    validateForm: function () {
+    validateForm: function() {
+      if ((this.username !== '') &&
+      (this.email !== '') &&
+      //check for empty password..might be related to if this.password = null..but research
+      (this.password === this.passwordVerify)
+      ) {
+        this.showForm = false;
+      } else{
+        this.showError = true;
+      }
       // Validate the form by checking the following values:
       // username must not be blank
       // email must not be blank
@@ -51,10 +95,9 @@ export default {
       //
       // When the form is validated, show the .success-message content
       // If the form is invalid, show the form error message
-
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -64,7 +107,8 @@ export default {
   padding: 1rem;
   color: #aa0000;
 }
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 
